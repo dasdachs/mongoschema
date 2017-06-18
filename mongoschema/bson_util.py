793b771e52201f91a506481afb5b6e868d99372f
dasdachs <jani.sumak@gmail.com>
2017-06-18 20:46:31 +0200

    Added a bson mapper to bson_utils

--- mongoschema/bson_util.py
+++ mongoschema/bson_util.py
@@ -7,6 +7,19 @@ import re
 
 # __all__ = [""]
 
+BSON_MAPPER = {
+        bson.objectid.ObjectId: "objectId",
+        bool: "bool",
+        datetime.date: "Date",
+        datetime.datetime: "Date",
+        bson.regex.Regex: "Regex",
+        str: "string",
+        int: "integer",
+        float: "float",
+        list: "array",
+        dict: "object",
+}
+
 def _get_int(value):
     """Encode python int type to bson int32/64."""
     if -2147483648 <= value <= 2147483647:
