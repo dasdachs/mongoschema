adcc997507a7b8585ab636997f08e779eac9a90f
dasDachs <jani.sumak@gmail.com>
2017-07-25 23:53:37 +0200

    Finished first round of tests for bson_utils

--- tests/test_bson_utils.py
+++ tests/test_bson_utils.py
@@ -1,8 +1,9 @@
 import datetime
+import math
 
 from bson.objectid import ObjectId
 
-from mongoschema.bson_utils import *
+from mongoschema.bson_utils import get_dtype, _get_int
 
 
 class TestMapper(object):
@@ -49,3 +50,49 @@ class TestMapper(object):
 
     def test_for_array_with_list(self):
         assert get_dtype(["test"]) == "array"
+
+    def test_for_array_with_python_array(self):
+        """TODO test with pythons array"""
+        pass
+
+    def test_for_array_with_np_array(self):
+        """TODO test with numpy arrays"""
+        pass
+
+    def test_for_object_with_empty_dict(self):
+        assert get_dtype({}) == "object"
+
+    def test_for_object_with_dict(self):
+        assert get_dtype({"a":"test"}) == "object"
+
+    def test_for_byte(self):
+        assert get_dtype(b"11") == "binData"
+
+class TestGetInt(object):
+    def test_for_int_with_zero(self):
+        assert _get_int(0) == "int"
+
+    def test_for_int_with_positive_integer(self):
+        assert _get_int(42) == "int"
+
+    def test_for_int_with_negative_integer(self):
+        assert _get_int(-42) == "int"
+
+    def test_for_int_with_pi(self):
+        assert _get_int(math.pi) == "int"
+
+    def test_for_int_with_positive_long(self):
+        assert _get_int(2147483648) == "long"
+
+    def test_for_int_with_border_positive_int(self):
+        assert _get_int(2147483647) != "long"
+
+    def test_for_int_with_negative_long(self):
+        assert _get_int(-2147483649) == "long"
+
+    def test_for_int_with_border_negative_int(self):
+        assert _get_int(-2147483647) != "long"
+
+    def test_for_int_with_infinity(self):
+        assert _get_int(math.inf) == "long"
+        assert _get_int(math.inf) != "int"
